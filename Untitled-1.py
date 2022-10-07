eeaa836a4dc4adc1a9438b92dba4b5129506c61e
nikitaazz <nikiokean2007@gmail.com>
2022-10-07 08:47:13 +0300

    Fised upload file

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,7 @@
-print("Hello world")
-z=0
-Vozrast=int(input("your age "))
-while Vozrast<18:
-    Vozrast=Vozrast+1
-    z=z+1
-print(z)
-print("ne lox ")
-print("artur krutoy ")
+n=int(input())
+while n>-0:
+  if n//2==0:
+    n=n/10+1
+  else:
+    n=n%10+1
+print(n)
