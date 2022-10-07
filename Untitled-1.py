490212350f1d9ba3694673ba731d43e61ba0a1c3
nikitaazz <nikiokean2007@gmail.com>
2022-10-07 17:28:27 +0300

    fixed uptade gfrsme

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,11 @@
-n=int(input())
-while n>-0:
-  if n//2==0:
-    n=n/10+1
-  else:
-    n=n%10+1
-print(n)
+import random
+spysok=[]
+lowest=10
+high=-10
+for i in range(20):
+  k=random.randint(-10,10)
+  if k>high:
+    high=k
+  if k<lowest:
+    lowest=k
+print(high-lowest)
