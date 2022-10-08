47a31647e83715874a2223d614b299aac3366fc5
nikitaazz <nikiokean2007@gmail.com>
2022-10-08 21:26:08 +0300

    Png
    Add png

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,5 @@
-import random
-spysok=[]
-lowest=10
-high=-10
-for i in range(20):
-  k=random.randint(-10,10)
-  if k>high:
-    high=k
-  if k<lowest:
-    lowest=k
-print(high-lowest)
+import eel
+
+eel.init("web")
+
+eel.start("main.html", size=(700,700))
